d81847127e778b63934d4d6b85789287febf918f
Ekky Patria <ekkypatria@gmail.com>
2020-01-24 13:52:51 +0700

    [cleanup] remove unrelated code in this step

--- my-svelte-project/src/2_img.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	let src = 'https://svelte.dev/tutorial/image.gif';
-	let imgAlt = 'Rick Astley';
-</script>
-
-<h2>step-2: image</h2>
-<!-- src={src} 	==>> ini tidak perlu ditulis sepanjang ini karena variablenya sudah sama dgn attributesnya-->
-<img {src} alt="{imgAlt} dances.">
--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,9 +1,6 @@
 <script>
 	// ambil name dari main.js
 	export let name
-  
-	import Training1 from './1_nested.svelte'
-	import Training2 from './2_img.svelte'
 
 	import Training3 from './3_event.svelte'
 	import Training32 from './3_event_advance.svelte'
@@ -12,14 +9,6 @@
 <main>
 	<h1><span class="blue"></span>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	
-	<!-- 1) nested components (termasuk @html komponen) -->
-	<hr/>
-	<Training1/>
-  
-  <!-- 2) image & shorthand buat attribute (jika nama variable sama) -->
-	<hr/>
-	<Training2/>
 
 	<!-- 3) event (click) & reactive & ternary condition sample -->
 	<hr/>
